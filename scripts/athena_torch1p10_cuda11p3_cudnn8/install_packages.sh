48f8049faeda151326630d4d93c7b914e163e289
jaewoo_park <park.jaewoo@aiv.ai>
2023-11-30 20:23:27 +0900

    chore: save

--- scripts/athena_torch1p10_cuda11p3_cudnn8/install_packages.sh
+++ scripts/athena_torch1p10_cuda11p3_cudnn8/install_packages.sh
@@ -1,1 +1,8 @@
+apt-get update
+apt-get upgrade
+pip uninstall -y torch
+pip uninstall -y torchvision
+pip uninstall -y torchaudio
+pip uninstall -y torchelastic
+pip install torch==1.10.0+cu111 torchvision==0.11.0+cu111 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
