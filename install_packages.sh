fc2c324c35b2971ca03f2e2b38f6fbf46ecaf2ca
roomo7time <julypraise@gmail.com>
2024-04-30 21:31:52 +0900

    feat: install export modules

--- install_packages.sh
+++ install_packages.sh
@@ -1,2 +1,4 @@
 pip install tabulate
 pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1 -f https://download.pytorch.org/whl/cu111/torch_stable.html
+pip install onnx==1.16.0
+pip install tensorrt==8.5.3.1
