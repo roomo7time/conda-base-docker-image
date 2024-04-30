f991d3b480ab144b195091871ac8b03036d1c0f5
roomo7time <julypraise@gmail.com>
2024-04-30 21:38:12 +0900

    feat: package

--- install_packages.sh
+++ install_packages.sh
@@ -1,4 +1,6 @@
 pip install tabulate
 pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1 -f https://download.pytorch.org/whl/cu111/torch_stable.html
+conda install -c pytorch faiss-gpu==1.7.3 -y
 pip install onnx==1.16.0
+pip install onnxruntime-gpu==1.16.3
 pip install tensorrt==8.5.3.1
